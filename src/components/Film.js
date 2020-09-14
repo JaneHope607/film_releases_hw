c5775ea8a6fe50d11a8a846a62320d905c315fd9
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-09-14 17:17:14 +0100

    film.js and FilmList completed

---
+++ src/components/Film.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Film extends Component {
+    render() {
+        return (
+            <>
+                <li className="item">
+        <a className="item-link" href={this.props.url}>{this.props.children}</a>
+                </li>
+            </>
+        )
+    }
+}
+
+
+export default Film;
---
+++ src/components/FilmList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Film from './Film'
+
+class FilmList extends Component {
+    render() {
+        const filmNodes = this.props.films.map((film) => {
+            return (
+            <Film key={film.id} url={film.url}>{film.name}</Film>
+            );
+        });
+
+        return (
+            <>
+                <ul>
+                    {filmNodes}
+                </ul>
+            </>
+        )
+    }
+}
+
+export default FilmList;
